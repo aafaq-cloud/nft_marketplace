ea2851d73537201ecdd62de8d25e986b55cd1771
Aafaq S <aafaqsikandar@gmail.com>
2023-04-05 14:11:01 +0500

    create routes for pages in nextjs file base routing

---
+++ pages/created-nfts.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// A page where we will be able to view our created NFTs
+const CreatedNFTs = () => <div>CreatedNFTs</div>;
+
+export default CreatedNFTs;
---
+++ pages/nft-details.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NFTDetails = () => <div>NFTDetails</div>;
+
+export default NFTDetails;
+ 
